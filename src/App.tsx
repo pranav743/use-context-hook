1c468041869fc8c74152a8f015ab5173c7a0ad91
Pranav <masterpranav07@gmail.com>
2025-08-19 13:01:49 +0530

    Added Progress bar and Reset Quiz Btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { QuizProvider } from './context/QuizContextProvider';
 import QuestionComponent from './components/Question/Question';
+import ProgressBar from './components/ProgressBar/ProgressBar';
 
 function App() {
   return (
     <div>
       <QuizProvider>
-        <h1>My React Template</h1>
+        <h1>Quiz App</h1>
+        <ProgressBar />
         <QuestionComponent />
       </QuizProvider>
     </div>
--- src/components/ScoreBoard/ScoreBoard.tsx
+++ src/components/ScoreBoard/ScoreBoard.tsx
@@ -2,10 +2,11 @@ import styles from './ScoreBoard.module.css';
 import useQuiz from '@/hooks/useQuiz';
 
 const ScoreBoard = () => {
-  const { score, questions } = useQuiz();
+  const { score, questions, resetQuiz } = useQuiz();
   return (
     <div className={styles.scoreboard}>
       <h1>{`${score} / ${questions.length}`}</h1>
+      <button onClick={resetQuiz}>Restart Quiz</button>
     </div>
   );
 };
