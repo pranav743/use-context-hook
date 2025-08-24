e028ed8f65772ae357438e6c87b4bc5a40c2aca0
Pranav <masterpranav07@gmail.com>
2025-08-24 21:29:51 +0530

    Project Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { QuizProvider } from './context/QuizContextProvider';
-import QuestionComponent from './components/Question/Question';
-import ProgressBar from './components/ProgressBar/ProgressBar';
 
 function App() {
   return (
     <div>
-      <QuizProvider>
-        <h1>Quiz App</h1>
-        <ProgressBar />
-        <QuestionComponent />
-      </QuizProvider>
+        <h1>Movie Explorer</h1>
     </div>
   );
 }
